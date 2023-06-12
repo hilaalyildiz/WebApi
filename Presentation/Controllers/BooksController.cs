fe763a02fe471a6bd077ac31b6fc2b4dea9f5bab
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2023-06-12 12:05:02 +0300

    Deleted try-catch

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -24,105 +24,69 @@ namespace Presentation.Controllers
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            try
-            {
-                var books = _manager.BookService.GetAllBooks(false);
-                return Ok(books);
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+            var books = _manager.BookService.GetAllBooks(false);
+            return Ok(books);
 
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
-                var book = _manager.BookService.GetOneBookById(id, false);
-                if (book is null)
-                    return NotFound();
 
-                return Ok(book);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var book = _manager.BookService.GetOneBookById(id, false);
+            if (book is null)
+                return NotFound();
+
+            return Ok(book);
         }
 
         [HttpPost]
         public IActionResult CreateOneBook([FromBody] Book book)
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest();
 
-                _manager.BookService.CreateOneBook(book);
+            if (book is null)
+                return BadRequest();
 
-                return StatusCode(201, book);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            _manager.BookService.CreateOneBook(book);
+
+            return StatusCode(201, book);
         }
+
         [HttpPut("{id:int}")]
         public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest();
 
-                _manager.BookService.UpdateOneBook(id, book);
+            if (book is null)
+                return BadRequest();
 
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.UpdateOneBook(id, book);
+
+            return NoContent();
+
         }
 
         [HttpDelete("{id:int}")]
         public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
-                _manager.BookService.DeleteOneBook(id, false);
-                return NoContent();
 
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.DeleteOneBook(id, false);
+            return NoContent();
+
         }
 
         [HttpPatch("{id:int}")]
         public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id,
             [FromBody] JsonPatchDocument<Book> bookPatch)
         {
-            try
-            {
-                var entity = _manager.BookService.GetOneBookById(id, true);
-                if (entity is null)
-                    return NotFound();
 
-                bookPatch.ApplyTo(entity);
-                _manager.BookService.UpdateOneBook(id, entity);
+            var entity = _manager.BookService.GetOneBookById(id, true);
+            if (entity is null)
+                return NotFound();
 
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            bookPatch.ApplyTo(entity);
+            _manager.BookService.UpdateOneBook(id, entity);
+
+            return NoContent();
         }
     }
 }
