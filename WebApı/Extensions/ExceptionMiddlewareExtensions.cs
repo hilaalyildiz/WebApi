6523848c619071bab8681da101126f52639b9b71
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2023-06-12 11:43:50 +0300

    Added ExceptionMiddleware

---
+++ WebApı/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Entitites.ErrorModels;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Contracts;
+using System.Net;
+
+namespace WebApi.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app,ILoggerService logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFuture = context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFuture is not null)
+                    {
+                        logger.LogError($"Something went wrong:{contextFuture.Error}");
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error"
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
