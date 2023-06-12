9ecc2a8be7261ff4a1665a0fd64353db53fbcc13
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2023-06-12 13:18:41 +0300

    Added Exceptions File

---
+++ Entitites/Exceptions/NotFound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites.Exceptions
+{
+    public abstract class NotFound : Exception
+    {
+        protected NotFound(string message) : base(message) 
+        {
+            
+        }
+    }
+    public sealed class BookNotFound : NotFound
+    {
+        public BookNotFound(int id) : base($"The book with id : {id} could not found")
+        {
+                
+        }
+    }
+}
